8734e9bdf8b20a5e9f328e4af1520d61a02e5b0b
Pham Nhu Quoc Trieu <phamnhuquoctrieu307@gmail.com>
2025-01-14 22:37:22 +0700

    #2 ex in chap6

--- practice.js
+++ practice.js
@@ -1,81 +1,69 @@
-const bai1 = () => {
-    for (var i = 1; i < 7; i++) {
-        console.log("#".repeat(i));
+// Bai1
+
+class Vec {
+    constructor(x, y) {
+      this.x = x;
+      this.y = y;
     }
-};
-// bai1();
-
-
-
-function bai2(size) {
-    let chessboard = "";
-    for (let row = 0; row < size; row++) {
-        let rowString = "";
-        for (let col = 0; col < size; col++) {
-            if ((row + col) % 2 === 0) {
-                rowString += "#";
-            } else {
-                rowString += " ";
-            }
-        }
-        chessboard += rowString + "\n";
-    }
-    return chessboard;
-}
-let size = 8;
-// console.log(bai2(size));
-
-const bai3 = () => Math.min(2 , 3)
-
-// console.log(bai3());
-
-function isEven(n) {
-   
-    if (n === 0) {
-        return true; 
-    } else if (n === 1) {
-        return false; 
-    } else if (n < 0) {
-        return false;
-    } else {
-       
-        return isEven(n - 2);
-    }
-}
-
-console.log(isEven(50));
-console.log(isEven(72)); 
-console.log(isEven(-1)); 
-
-
-const countChar = (string, char) => {
-    let count = 0;
-    for (let i = 0; i < string.length; i++) {
-        if (string[i] === char) {
-            count++;
-        }
-    }
-    return count;
-}
-
-function countBs(string) {
-    return countChar(string, 'B');
-}
-
-console.log(countBs("BBCBBAB")); 
-console.log(countChar("hello world", 'o')); 
-
-
-let users = [
-    { id: 1, name: "Alex" },
-    { id: 2, name: "John" },
-    { id: 3, name: "Anna" },
-  ];
   
-  let user = users.find((item) => item.id === 2);
-
-    console.log(user);   //{ id: 2, name: 'John' }
-
-  const bin = users.findIndex( (user) => user.name === "John")
+    plus(other) {
+      return new Vec(this.x + other.x, this.y + other.y);
+    }
   
-  console.log(bin); //1
+    minus(other) {
+      return new Vec(this.x - other.x, this.y - other.y);
+    }
+  
+    get length() {
+      return Math.sqrt(this.x ** 2 + this.y ** 2);
+    }
+  }
+  
+  let vec1 = new Vec(3, 4);
+  let vec2 = new Vec(1, 2);
+  
+  console.log(vec1.plus(vec2));
+  console.log(vec1.minus(vec2)); 
+  console.log(vec1.length); 
+  console.log(vec2.length);
+  
+
+  //Bai2
+
+  class Group {
+     constructor() {
+        this.group = []
+     }
+     add(value) {
+        if (!this.has(value)) {
+           this.group.push(value)
+        }
+     }
+     delete(value){
+            this.group = this.group.filter(v => v !== value)
+     }
+     has(value){
+         return this.group.includes(value)
+     }
+    //tạo 1 cái mới thông qua 1`iterable
+     static from(iterable) {
+        let group = new Group();
+        for (let value of iterable) {
+          group.add(value);
+        }
+        return group;
+      }
+  } 
+  let group1 = new Group();
+group1.add(1);
+group1.add(2);
+console.log(group1.has(1)); 
+console.log(group1.has(3)); 
+
+group1.delete(1);
+console.log(group1.has(1)); 
+
+let array = [1, 2, 3, 4, 5];
+let group2 = Group.from(array);
+console.log(group2.has(3)); 
+console.log(group2.has(6));
